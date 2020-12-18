7df41c5bb7ed8ab083cea8a8b233d31170b9f8ad
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-12-18 13:35:54 +0000

    feat: simplified

--- contracts/strategies/CPoolHarvest.sol
+++ contracts/strategies/CPoolHarvest.sol
@@ -1,10 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.7.0;
 
-import "@openzeppelin/contracts/math/SafeMath.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+// import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 
 import "../interfaces/IComptroller.sol";
 import "../interfaces/ICToken.sol";
@@ -14,61 +12,57 @@ import "../CPool.sol";
 
 /// @dev CPoolStrategy is a CPool that allows for Comp rewards to be harvested and converted to cDai, which is added to the reserves.
 contract CPoolStrategy is CPool {
-    using SafeMath for uint256;
 
-    address public immutable comp;
-    address public immutable dai;
-    ICToken public immutable cdai;
-
+    IERC20 public immutable comp;
     IUniswapV2Router public immutable uniswap;
-    address public immutable weth; // used for comp <> weth <> dai route
-
     IComptroller public immutable comptroller;
 
-    event Harvested(uint256 wantEarned, uint256 lifetimeEarned);
+    /// @dev Comp harvested, and Dai obtained by its sale
+    event Harvested(uint256 comp, uint256 dai);
 
     constructor(ICToken cDai_, IFYDai fyDai_, IComptroller comptroller_, IUniswapV2Router uniswap_, string memory name_, string memory symbol_)
         CPool(cDai_, fyDai_, name_, symbol_)
     {
         comptroller = comptroller_;
-        comp = comptroller_.getCompAddress();
-        dai = cDai_.underlying();
+        comp = IERC20(comptroller_.getCompAddress());
         uniswap = uniswap_;
-        weth = uniswap_.WETH();
     }
 
+    /// @dev Claim comp, sell it for Dai, and mint cDai which remains in the CPool reserves
     function harvest() public {
         // require(msg.sender == strategist || msg.sender == governance, "!authorized");
+        IERC20 dai = IERC20(cDai.underlying());
 
-        uint256 _comp = claimComp();
-        if (_comp > 0) {
+        uint256 compAmount = claimComp();
+        if (compAmount > 0) {
             // IERC20(comp).safeApprove(uni, 0);
-            IERC20(comp).approve(address(uniswap), _comp);
+            comp.approve(address(uniswap), compAmount);
 
             address[] memory path = new address[](3);
-            path[0] = comp;
-            path[1] = weth;
-            path[2] = dai;
+            path[0] = address(comp);
+            path[1] = uniswap.WETH();
+            path[2] = address(dai);
 
-            uniswap.swapExactTokensForTokens(_comp, uint256(0), path, address(this), block.timestamp.add(1800));
+            uniswap.swapExactTokensForTokens(compAmount, uint256(0), path, address(this), block.timestamp + 1800); // Unlikely to overflow
         }
-        uint256 _dai = IERC20(dai).balanceOf(address(this));
-        if (_dai > 0) {
-            IERC20(dai).approve(address(cdai), _dai);
-            cdai.mint(_dai);
+        uint256 daiAmount = dai.balanceOf(address(this));
+        if (daiAmount > 0) {
+            dai.approve(address(cDai), daiAmount);
+            cDai.mint(daiAmount);
         }
 
-        emit Harvested(_dai, _dai);
+        emit Harvested(compAmount, daiAmount);
     }
 
+    /// @dev Claim all due Comp
     function claimComp() private returns (uint256) {
         address[] memory holders = new address[](1);
         holders[0] = address(this);
         address[] memory cTokens = new address[](1);
-        cTokens[0] = address(cdai);
+        cTokens[0] = address(cDai);
 
         comptroller.claimComp(holders, cTokens, false, true);
 
-        return IERC20(comp).balanceOf(address(this));
+        return comp.balanceOf(address(this));
     }
 }
